3995b24ae6ef662895a9cbad8004d00d6e06e152
Frank Dininno <frankdininno@gmail.com>
2018-10-26 11:31:32 -0700

    needed help with hub, parameters are not being followed

--- src/com/company/Hud.java
+++ src/com/company/Hud.java
@@ -1,4 +1,22 @@
 package com.company;
 
+import java.util.HashMap;
+
 public class Hud {
+    public static Character char_items = new Character();
+    //create items
+    HashMap<String, Integer> itemGeneric = new HashMap<>();
+
+
+
+    //end of items
+    //adds attributes to items and adds it to the characters inventory(char.items)
+    public static void add_attributes_to_item(HashMap<String, Integer> item, String modifying, int value){
+        item.put(modifying, value);
+        char_items.char_items.add(item);
+    }
+
+    public static void main(String[]args){
+        add_items_to_inventory(itemGeneric, strength, 3);
+    }
 }
