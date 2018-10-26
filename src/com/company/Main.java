fbebcf75850e88e24bba17f351af377383dd8995
Dylan Starink <dylan@starink.com>
2018-10-26 08:27:55 -0700

    Added Design Standards

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,7 +1,13 @@
 package com.company;
+import com.company.*;
 
 public class Main{
     public static void main(String[]args){
-        initialize_status();
+
+    	Character character1 = new Character();
+
+    	character1.changeStats("strength",4);
+
+    	System.out.println(character1.char_stats.get("strength"));
     }
 }
