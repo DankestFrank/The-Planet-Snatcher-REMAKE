2cf16bea335f1ee7afcbdc0be64b666695306c9d
Dylan Starink <dylan@starink.com>
2018-10-30 07:32:29 -0700

    Added Sector Functions

--- src/com/company/locations/Sector.java
+++ src/com/company/locations/Sector.java
@@ -9,14 +9,51 @@ public class Sector {
 
 	String openingText;
 
+	spaceType type = spaceType.DEFAULT;
+
 	ArrayList<ItemTemplate> items = new ArrayList<>();
 
 	ArrayList<Sector> connectedNodes = new ArrayList<>();
 
+	public void setName(String name) {
+		this.name = name;
+	}
 
+	public void setOpeningText(String openingText) {
+		this.openingText = openingText;
+	}
+
+	public void setType(spaceType type) {
+		this.type = type;
+	}
+
+	private void addConnectedNode(Sector node){
+		node.connectedNodes.add(this);
+		this.connectedNodes.add(node);
+	}
+
+	public Sector addChildNode(String name){
+		Sector newChildNode = new Sector(name);
+		addConnectedNode(newChildNode);
+
+		return newChildNode;
+	}
+
+
+	enum spaceType{
+		DEFAULT,
+		BUILDING,
+		HOUSE,
+		OUTSIDE_DESERT,
+		CAVE,
+		OUTSIDE_PLAINS
+
+	}
 
 
 	Sector(String name){
-
+		this.name = name;
 	}
+
+
 }
