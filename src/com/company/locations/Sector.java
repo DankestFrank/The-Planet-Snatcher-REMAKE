35232e5cc8d5aee4a5807b92bd12d0be7d342fd7
Frank Dininno <frankdininno@gmail.com>
2018-10-31 20:41:15 -0700

    nvm is saved!
    autosave! yes! YESS YYYYYYYEEEEEESS!

--- src/com/company/locations/Sector.java
+++ src/com/company/locations/Sector.java
@@ -1,11 +1,31 @@
 package com.company.locations;
 
+import com.company.Status;
 import com.company.inventory.ItemTemplate;
 
 import java.util.ArrayList;
 
 public class Sector {
 	String name;
+
+	public static int desertCountMax = 0;
+	public static int plainsCountMax = 0;
+
+	public static int subsettingNodeCount = 0; //advances subsetting every time this var reaches 1-3 or more 
+
+	//'()' = example
+	public static String selectSetting;
+	// you roam the (plains) [couple nodes]
+	public static String selectedLandmark;
+	// you find a (city) of tag selectedSetting [bunch of nodes because many building for say]
+	public static String selectedstructure;
+	// you reach a dead end of a (house) of tag selectedLandMark [few nodes]
+
+	public static String currentSubsetting;
+
+	//whenever you reach a land mark, it is through a node leading past and into it
+
+
 
 	String openingText;
 
@@ -30,16 +50,48 @@ public class Sector {
 	public String displayNameMessage;
 
 	private void addConnectedNode(Sector node){
-		node.connectedNodes.add(this);
-		this.connectedNodes.add(node);
+		if(node.equals((null))) {
+		}
+		else {
+			node.connectedNodes.add(this);
+			this.connectedNodes.add(node);
+		}
 	}
 
 	public Sector addChildNode(String name){
 		Sector newChildNode = new Sector(name);
+		//random customization desert > outside or in a city >
 		addConnectedNode(newChildNode);
-
 		return newChildNode;
 	}
+
+	public String desert(){
+		desertCountMax++;
+		if(desertCountMax <=100) {
+			return "desert" + desertCountMax;
+		}
+		else {
+			return null;
+		}
+	}
+	public String plains(){
+		desertCountMax++;
+		if(plainsCountMax <=100) {
+			return "plains" + plainsCountMax;
+		}
+		else {
+			return null;
+		}
+	}
+
+	public void generateChildren(){
+		Sector firstNode = new Sector("Start Node");
+		Status.currentNode = firstNode;
+		for(int i=0 ; i<((Math.random()*2)+1); i++){
+			addChildNode(currentSubsetting);
+		}
+	}
+
 
 
 	enum spaceType{
@@ -53,8 +105,9 @@ public class Sector {
 	}
 
 
-	Sector(String name){
+	public Sector(String name){
 		this.name = name;
+		generateChildren();
 	}
 
 
